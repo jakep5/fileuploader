5547a8bbd3b532f78f147215f3bf4744048bef2e
jakep5 <jakepagel1@yahoo.com>
2019-10-21 11:35:56 -0500

    code FilterableList component

---
+++ src/components/FilterOptions/FilterOptions.js
@@ -0,0 +1,34 @@
+import './FilterOptions.css';
+
+class FilterOptions extends React.Component {
+    render() {
+        return (
+            <div className="FilterOptions">
+                <div className="FilterOptions__option">
+                    <label htmlFor="filter_all">
+                        <input type="radio" value="All" id="filter_all" name="filter"></input>
+                            All
+                    </label>
+                </div>
+                <div className="FilterOptions__option">
+                    <label htmlFor="filter_uploaded">
+                        <input type="radio" value="Uploaded" id="filter_uploaded" name="filter"/>
+                        Uploaded
+                    </label>
+                </div>
+                <div className="FilterOptions__option">
+                    <label htmlFor="filter_synched">
+                        <input type="radio" value="Synced" id="filter_synced" name="filter"/>
+                        Synched
+                    </label>
+                </div>
+                <div className="FilterOptions__option">  
+                    <label htmlFor="filter_new">
+                        <input type="radio" value="New" id="filter_new" name="filter"/>
+                        New
+                    </label>
+                </div>
+            </div>
+        )
+    }
+}
